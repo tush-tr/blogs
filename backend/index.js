114208ae11a3620a2385227a7778fc6a2837ca93
Tushar Rajpoot <tusharrajput604@gmail.com>
2021-12-13 14:23:13 +0000

    backend api

---
+++ backend/index.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const mongoUrl = process.env.MONGOURL
+mongoose.connect(mongoUrl, { useNewUrlParser: true });
+const postSchema = new mongoose.Schema({
+    blogid: String,
+    content: String,
+    title: String,
+    tag: String,
+    image: String
+})
+
+const Article = mongoose.model('Article', postSchema);
+
+app.get("/articles", (req, res) => {
+    Article.find((err, findItems) => {
+        res.json(findItems)
+    })
+})
+app.get("/compose", (req, res) => {
+    res.render("compose");
+})
+
+app.post("/compose",(req,res)=>{
+    let postContent = JSON.stringify(req.body.content)
+    const postForAdd = new Article({
+        blogid: req.body.blogid,
+        content: postContent,
+        title: req.body.title,
+        tag: req.body.tag,
+        image: req.body.image
+    })
+    postForAdd.save();
+    console.log("Saved Successfully")
+    res.redirect("/articles");
+})
+
+
+app.get("/articles/:id", async (req, res) => {
+    try {
+        const post = await Article.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+
+app.listen(3001, () => {
+    console.log("Server is up")
+})
+
