702f0cb5881598835e01b7ec0102260718b2d363
Tushar Rajpoot <tusharrajput604@gmail.com>
2021-12-13 16:01:19 +0000

    added procfile

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const _ = require("lodash");
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const port = process.env.port || 3001
+const port = process.env.PORT || 3001
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
